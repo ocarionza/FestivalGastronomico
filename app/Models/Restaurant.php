d6b3d6974f3891127e2ea3b408926bf44f3b0b04
BrayanOcampo <brayan.ocampo.c7@gmail.com>
2021-09-19 23:11:22 -0500

    Modelos (Categoria, Restaurante, Cometario)

---
+++ app/Models/Restaurant.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'city',
+        'Phone',
+        'category_id',
+        'delivery',
+        'schedule',
+        'latitude',
+        'longitude',
+        'logo',
+        'facebook',
+        'twitter',
+        'instagram',
+        'youtube',
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+}
+
